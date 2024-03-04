28bf7ab09415f47a7d3f41e28ab7c6d11ae4a69e
vzanbr <128941678+vzanbr@users.noreply.github.com>
2024-03-04 13:21:01 -0300

    feature: Implementado modo claro e escuro do site

---
+++ main.js
@@ -0,0 +1,7 @@
+let trilho = document.getElementById('trilho')
+let body = document.querySelector('body')
+
+trilho.addEventListener('click', () =>{
+    trilho.classList.toggle('dark')
+    body.classList.toggle('dark')
+})
